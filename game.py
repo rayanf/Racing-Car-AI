363bf59f0b3b074c6b8001bbf9a84a3c5ef6c250
rayanf <rayanforsat@gmail.com>
2021-09-18 01:04:44 +0430

    init walls

--- game.py
+++ game.py
@@ -12,13 +12,17 @@ class Game:
         self.fps = 10
         self.changeDegree = 10
         self.changeSpeed = 2
-        self.screen = pygame.display.set_mode([self.w,self.h])  
-        self.screen.fill((50, 153, 213))
+                
+        ww = pygame.display.Info().current_w
+        wh = pygame.display.Info().current_h
+        self.screen = pygame.display.set_mode((ww, wh))
+        self.screen.fill((255,0,0))
+        self.screen.blit(pygame.image.load('track.png'),(0,0))
         self.clock = pygame.time.Clock()  
 
         self.cars = []
         self.carsObjects = pygame.sprite.Group()
-
+        self.track = pygame.image.load('track.png')
         
 
     def add_car(self,pos,img):
@@ -30,16 +34,31 @@ class Game:
         for i in range(len(self.cars)):
             car = self.cars[i]
             move = moves[i]
+            if not self.check_road(car.rect.center):
+                car.speed = 0
+                break
 
+            car.move(move)   
+
+        game.update_screen()
 
 
     def update_screen(self):
-        self.screen.fill((50, 153, 213))
+        self.screen.fill((255,0,0))
+        self.screen.blit(pygame.image.load('track.png'),(0,0))
 
         self.carsObjects.draw(self.screen)        
         pygame.display.update()  
         pygame.display.flip()
 
+    def check_road(self,point):
+        clr = self.track.get_at(point)
+        # print(clr)
+        # print(point)
+
+        if clr == (100,100,100,255): 
+            return True
+        else: return False
 
 class Car(pygame.sprite.Sprite):
     def __init__(self,pos,img):
@@ -69,24 +88,22 @@ class Car(pygame.sprite.Sprite):
 
         self.image = pygame.transform.rotate(self.image,change_degree*-1)
 
+
 if __name__ == '__main__':
     game = Game()
-    game.add_car((200,200),'car_1.png')
+    game.add_car((100,200),'car_1.png')
     
     # while True:
     for i in range(10):
-        game.cars[0].move('right')
-        game.cars[0].move('forward')
-        game.update_screen()
+        game.one_step(['right'])
+        game.one_step(['forward'])
         game.clock.tick(game.fps)  
 
     for i in range(10):
-        game.cars[0].move('backward')
-        game.update_screen()
+        game.one_step(['backward'])
         game.clock.tick(game.fps)  
 
     for i in range(10):
-        game.cars[0].move('left')
-        game.cars[0].move('forward')
-        game.update_screen()
+        game.one_step(['left'])
+        game.one_step(['forward'])
         game.clock.tick(game.fps)  
