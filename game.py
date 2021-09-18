a00b39ae8c94cbc96de5e61b36bdc0b83695adc3
rayanf <rayanforsat@gmail.com>
2021-09-18 21:05:00 +0430

    fix rotate bug

--- game.py
+++ game.py
@@ -9,13 +9,13 @@ class Game:
     def __init__(self):
         self.w = 1500
         self.h = 800
-        self.fps = 10
+        self.fps = 20
         self.changeDegree = 10
-        self.changeSpeed = 2
+        self.changeSpeed = 1
                 
         ww = pygame.display.Info().current_w
         wh = pygame.display.Info().current_h
-        self.screen = pygame.display.set_mode((ww, wh))
+        self.screen = pygame.display.set_mode((1200,750))
         self.screen.fill((255,0,0))
         self.screen.blit(pygame.image.load('track.png'),(0,0))
         self.clock = pygame.time.Clock()  
@@ -34,9 +34,9 @@ class Game:
         for i in range(len(self.cars)):
             car = self.cars[i]
             move = moves[i]
-            if not self.check_road(car.rect.center):
-                car.speed = 0
-                break
+            # if not self.check_road(car.rect.center):
+            #     car.speed = 0
+                # break
 
             car.move(move)   
 
@@ -44,10 +44,11 @@ class Game:
 
 
     def update_screen(self):
-        self.screen.fill((255,0,0))
-        self.screen.blit(pygame.image.load('track.png'),(0,0))
+        self.screen.fill((255, 255, 102))
+        # self.screen.blit(pygame.image.load('track.png'),(0,0))
+        self.screen.blit(self.cars[0].rotatedimage,self.cars[0].rect.center)
 
-        self.carsObjects.draw(self.screen)        
+        # self.carsObjects.draw(self.screen)        
         pygame.display.update()  
         pygame.display.flip()
 
@@ -61,9 +62,11 @@ class Game:
         else: return False
 
 class Car(pygame.sprite.Sprite):
-    def __init__(self,pos,img):
+    def __init__(self,pos,color):
         pygame.sprite.Sprite.__init__(self)  
-        self.image = pygame.image.load(img)
+        self.image = pygame.Surface([35,20],pygame.SRCALPHA)
+        self.image.fill(color)
+        self.rotatedimage = self.image
         self.rect = self.image.get_rect()  
         self.rect.center = pos
         self.speed = 0
@@ -75,7 +78,6 @@ class Car(pygame.sprite.Sprite):
         for i in range(1,3):
             if move == self.moves[i]:
                 self.degree += game.changeDegree * [-1, 1][i-1]
-                change_degree = game.changeDegree * [-1, 1][i-1]
 
         if move == self.moves[0]:
             self.speed += game.changeSpeed * 1
@@ -86,12 +88,14 @@ class Car(pygame.sprite.Sprite):
         self.rect.x += self.speed * math.cos(math.radians(self.degree))
         self.rect.y += self.speed * math.sin(math.radians(self.degree))
 
-        self.image = pygame.transform.rotate(self.image,change_degree*-1)
+        self.rotatedimage = pygame.transform.rotate(self.image,self.degree*-1)
 
 
 if __name__ == '__main__':
+    color = (100,149,237)
+    
     game = Game()
-    game.add_car((100,200),'car_1.png')
+    game.add_car((100,200),color)
     
     # while True:
     for i in range(10):
@@ -105,5 +109,8 @@ if __name__ == '__main__':
 
     for i in range(10):
         game.one_step(['left'])
+        game.one_step(['forward'])
+        game.clock.tick(game.fps)  
+    for i in range(20):
         game.one_step(['forward'])
         game.clock.tick(game.fps)  
